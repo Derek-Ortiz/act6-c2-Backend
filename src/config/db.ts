27c08276d0438e505f7c6a938df3d4484f23b5d9
Derek-Ortiz <243742@ids.upchiapas.edu.mx>
2026-03-06 05:29:52 -0600

    Cambio de conexiones

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,11 +1,10 @@
 import { Pool, QueryResult } from 'pg';
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT) || 5432,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 export const query = (text: string, params?: any[]): Promise<QueryResult> => {
